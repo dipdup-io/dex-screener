f9ac81a3f14b36cfdb3bbf0d9bd21d6659112f1c
Lev Gorodetskiy <github@droserasprout.space>
2025-01-14 12:14:22 -0300

    models

--- models/__init__.py
+++ models/__init__.py
@@ -1,24 +1,182 @@
+from enum import Enum
+
 from dipdup import fields
 from dipdup.models import Model
 
+# interface Block {
+#   blockNumber: number;
+#   blockTimestamp: number;
+#   metadata?: Record<string, string>;
+# }
+
+# {
+#   "block": {
+#     "blockNumber": 100,
+#     "blockTimestamp": 1698126147
+#   }
+# }
+
+
+class Block(Model):
+    block_number = fields.IntField(primary_key=True)
+    block_timestamp = fields.IntField()
+    metadata = fields.JSONField(null=True)
+
+
+# interface Asset {
+#   id: string;
+#   name: string;
+#   symbol: string;
+#   totalSupply?: string | number;
+#   circulatingSupply?: string | number;
+#   coinGeckoId?: string;
+#   coinMarketCapId?: string;
+#   metadata?: Record<string, string>;
+# }
+
+# {
+#   "asset": {
+#     "id": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+#     "name": "Wrapped Ether",
+#     "symbol": "WETH",
+#     "totalSupply": 10000000,
+#     "circulatingSupply": 900000,
+#     "coinGeckoId": "ether",
+#     "coinMarketCapId": "ether"
+#   }
+# }
+
 
 class Asset(Model):
-    class Meta:
-        table = 'asset'
-        model = 'models.Asset'
-
     id = fields.IntField(primary_key=True)
     name = fields.TextField()
+    symbol = fields.TextField()
+    total_supply = fields.IntField(null=True)
+    circulating_supply = fields.IntField(null=True)
+    coin_gecko_id = fields.TextField(null=True)
+    coin_market_cap_id = fields.TextField(null=True)
+    metadata = fields.JSONField(null=True)
 
 
-class Event(Model):
-    class Meta:
-        table = 'event'
-        model = 'models.Event'
+# interface Pair {
+#   id: string;
+#   dexKey: string;
+#   asset0Id: string;
+#   asset1Id: string;
+#   createdAtBlockNumber?: number;
+#   createdAtBlockTimestamp?: number;
+#   createdAtTxnId?: string;
+#   creator?: string;
+#   feeBps?: number;
+#   pool?: {
+#     id: string;
+#     name: string;
+#     assetIds: string[];
+#     pairIds: string[];
+#     metadata?: Record<string, string>;
+#   };
+#   metadata?: Record<string, string>;
+# }
 
+# {
+#   "pair": {
+#     "id": "0x11b815efB8f581194ae79006d24E0d814B7697F6",
+#     "dexKey": "uniswap",
+#     "asset0Id": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+#     "asset1Id": "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+#     "createdAtBlockNumber": 100,
+#     "createdAtBlockTimestamp": 1698126147,
+#     "createdAtTxnId": "0xe9e91f1ee4b56c0df2e9f06c2b8c27c6076195a88a7b8537ba8313d80e6f124e",
+#     "feeBps": 100
+#   }
+# }
+
+
+class Pair(Model):
     id = fields.IntField(primary_key=True)
-    who = fields.TextField()
-    asset_id = fields.IntField()
-    asset_out = fields.IntField()
-    amount_id = fields.TextField()
-    amount_out = fields.TextField()
+    dex_key = fields.TextField()
+    asset_0_id = fields.IntField()
+    asset_1_id = fields.IntField()
+    created_at_block_number = fields.IntField(null=True)
+    created_at_block_timestamp = fields.IntField(null=True)
+    created_at_txn_id = fields.TextField(null=True)
+    creator = fields.TextField(null=True)
+    fee_bps = fields.IntField(null=True)
+    metadata = fields.JSONField(null=True)
+
+
+# interface SwapEvent {
+#   eventType: "swap";
+#   txnId: string;
+#   txnIndex: number;
+#   eventIndex: number;
+#   maker: string;
+#   pairId: string;
+#   asset0In?: number | string;
+#   asset1In?: number | string;
+#   asset0Out?: number | string;
+#   asset1Out?: number | string;
+#   priceNative: number | string;
+#   reserves?: {
+#     asset0: number | string;
+#     asset1: number | string;
+#   };
+#   metadata?: Record<string, string>;
+# }
+
+
+class SwapEventType(Enum):
+    swap = 'swap'
+
+
+class SwapEvent(Model):
+    event_type = fields.EnumField(SwapEventType, primary_key=True)
+    txn_id = fields.TextField(primary_key=True)
+    txn_index = fields.IntField(primary_key=True)
+    event_index = fields.IntField(primary_key=True)
+    maker = fields.TextField()
+    pair_id = fields.IntField()
+    asset_0_in = fields.IntField(null=True)
+    asset_1_in = fields.IntField(null=True)
+    asset_0_out = fields.IntField(null=True)
+    asset_1_out = fields.IntField(null=True)
+    price_native = fields.IntField()
+    reserves_asset_0 = fields.IntField(null=True)
+    reserves_asset_1 = fields.IntField(null=True)
+    metadata = fields.JSONField(null=True)
+
+
+# interface JoinExitEvent {
+#   eventType: "join" | "exit";
+#   txnId: string;
+#   txnIndex: number;
+#   eventIndex: number;
+#   maker: string;
+#   pairId: string;
+#   amount0: number | string;
+#   amount1: number | string;
+#   reserves?: {
+#     asset0: number | string;
+#     asset1: number | string;
+#   };
+#   metadata?: Record<string, string>;
+# }
+
+
+class JoinExitEventType(Enum):
+    join = 'join'
+    exit = 'exit'
+
+
+class JoinExitEvent(Model):
+    event_type = fields.EnumField(JoinExitEventType, primary_key=True)
+    txn_id = fields.TextField(primary_key=True)
+    txn_index = fields.IntField(primary_key=True)
+    event_index = fields.IntField(primary_key=True)
+    maker = fields.TextField()
+    pair_id = fields.IntField()
+    amount_0 = fields.IntField()
+    amount_1 = fields.IntField()
+    reserves_asset_0 = fields.IntField(null=True)
+    reserves_asset_1 = fields.IntField(null=True)
+    metadata = fields.JSONField(null=True)
