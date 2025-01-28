16291ed9d5a8499cfeabcde0564b3ffd0b904a12
Lev Gorodetskiy <github@droserasprout.space>
2025-01-28 15:20:18 -0300

    comment on_restart

--- hooks/on_restart.py
+++ hooks/on_restart.py
@@ -1,7 +1,7 @@
 from dipdup.context import HookContext
 
-from dex_screener import models
-from models import NATIVE_ASSET_ID
+# from dex_screener import models
+# from models import NATIVE_ASSET_ID
 
 
 async def on_restart(
@@ -9,15 +9,15 @@ async def on_restart(
 ) -> None:
     await ctx.execute_sql_script('on_restart')
 
-    model, created = await models.Asset.get_or_create(
-        id=NATIVE_ASSET_ID,
-        defaults={
-            'name': ctx.config.custom['native_token']['name'],
-            'symbol': ctx.config.custom['native_token']['symbol'],
-            'metadata': {
-                'decimals': ctx.config.custom['native_token']['decimals'],
-            },
-        },
-    )
-    if created:
-        ctx.logger.info('Creating native asset: %s', model.get_repr())
+    # model, created = await models.Asset.get_or_create(
+    #     id=NATIVE_ASSET_ID,
+    #     defaults={
+    #         'name': ctx.config.custom['native_token']['name'],
+    #         'symbol': ctx.config.custom['native_token']['symbol'],
+    #         'metadata': {
+    #             'decimals': ctx.config.custom['native_token']['decimals'],
+    #         },
+    #     },
+    # )
+    # if created:
+    #     ctx.logger.info('Creating native asset: %s', model.get_repr())
