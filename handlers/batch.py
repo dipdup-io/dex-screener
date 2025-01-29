e3b36ed50edb7bcdaf41433c501e5638b107179a
Lev Gorodetskiy <github@droserasprout.space>
2025-01-29 18:15:11 -0300

    fix batch handler

--- handlers/batch.py
+++ handlers/batch.py
@@ -10,24 +10,10 @@ async def batch(
 ) -> None:
     from dex_screener.models import Block
 
-    batch_levels = []
     for handler in handlers:
-        if handler.level not in batch_levels:
-            block = await Block.create(
-                level=handler.level,
-                timestamp=handler.args[0].data.header['timestamp'] // 1000,
-            )
-            batch_levels.append(handler.level)
-
         await ctx.fire_matched_handler(handler)
 
-    if block is not None:
-        await block.fetch_related('dex_assets', 'dex_pairs', 'dex_swap_events')
-        if all(
-            [
-                not block.dex_assets.related_objects,
-                not block.dex_pairs.related_objects,
-                not block.dex_swap_events.related_objects,
-            ]
-        ):
-            await block.delete()
+    await Block.create(
+        level=handler.level,
+        timestamp=handler.args[0].data.header['timestamp'] // 1000,
+    )
