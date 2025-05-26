bb155f7a8cd0058b429d6ee87756c6a5319ea0a1
Igor Sereda <sereda.igor.s@gmail.com>
2025-05-26 07:58:28 +0300

    Added EventBuffer

--- reserves/sql/on_refresh_history/01_refresh_balance.sql
+++ reserves/sql/on_refresh_history/01_refresh_balance.sql
@@ -19,32 +19,40 @@ CREATE UNIQUE INDEX tmp_new_balance_update_event_pkey
 ALTER TABLE tmp_new_balance_update_event
     ADD PRIMARY KEY USING INDEX tmp_new_balance_update_event_pkey;
 CREATE INDEX idx_tmp_new_balance_update_event_asset_account
-    ON tmp_new_balance_update_event USING hash (asset_account);
-CREATE INDEX idx_tmp_new_balance_update_event_asset_account_btree
-    ON tmp_new_balance_update_event USING btree (asset_account);
+    ON tmp_new_balance_update_event USING btree (asset_account ASC);
 
 
 DROP TABLE IF EXISTS tmp_new_balance_key;
 CREATE TEMP TABLE tmp_new_balance_key AS
-SELECT DISTINCT asset_account
-    FROM tmp_new_balance_update_event;
+SELECT DISTINCT ON (asset_account) asset_account
+    FROM tmp_new_balance_update_event
+    ORDER BY asset_account;
 CREATE INDEX idx_tmp_new_balance_key
-    ON tmp_new_balance_key USING hash (asset_account);
-CREATE INDEX idx_tmp_new_balance_key_btree
-    ON tmp_new_balance_key USING btree (asset_account);
+    ON tmp_new_balance_key USING btree (asset_account ASC);
 
 
 DROP TABLE IF EXISTS tmp_latest_balance;
 CREATE TEMP TABLE tmp_latest_balance AS
-    SELECT
-    last(bh.id, bh.id) AS id,
-    bh.asset_account,
-    last(balance, bh.id) AS latest_balance
-    FROM balance_history AS bh
-         JOIN tmp_new_balance_key AS ub USING (asset_account)
-    GROUP BY bh.asset_account;
-CREATE INDEX idx_tmp_latest_balance_btree
-    ON tmp_latest_balance USING btree (asset_account);
+SELECT DISTINCT ON (asset_account)
+    asset_account,
+    balance AS latest_balance
+    FROM balance_history
+    ORDER BY asset_account, id DESC;
+CREATE INDEX idx_tmp_latest_balance
+    ON tmp_latest_balance USING btree (asset_account ASC);
+
+
+DROP TABLE IF EXISTS tmp_new_latest_balance;
+CREATE TEMP TABLE tmp_new_latest_balance AS
+SELECT
+    asset_account,
+    latest_balance
+    FROM tmp_latest_balance AS lb
+    JOIN tmp_new_balance_key AS ub
+        USING (asset_account)
+    ORDER BY asset_account;
+CREATE INDEX idx_tmp_new_latest_balance
+    ON tmp_new_latest_balance USING btree (asset_account ASC);
 
 
 DROP TABLE IF EXISTS tmp_new_balance_update_with_latest_balance;
@@ -57,13 +65,15 @@ SELECT
     bue.balance_update,
     lb.latest_balance
     FROM tmp_new_balance_update_event AS bue
-        LEFT JOIN tmp_latest_balance AS lb USING (asset_account);
+        LEFT JOIN tmp_new_latest_balance AS lb USING (asset_account);
 CREATE UNIQUE INDEX tmp_new_balance_update_with_latest_balance_pkey
     ON tmp_new_balance_update_with_latest_balance USING btree (id ASC);
 ALTER TABLE tmp_new_balance_update_with_latest_balance
     ADD PRIMARY KEY USING INDEX tmp_new_balance_update_with_latest_balance_pkey;
-CREATE INDEX idx_tmp_new_balance_update_with_latest_balance_key_btree
+CREATE INDEX idx_tmp_new_balance_update_with_latest_balance_key
     ON tmp_new_balance_update_with_latest_balance USING btree (asset_account);
+CREATE INDEX idx_tmp_new_balance_update_with_latest_balance_distinct
+    ON tmp_new_balance_update_with_latest_balance USING btree (asset_account, id);
 
 
 WITH bucs AS (
@@ -93,4 +103,5 @@ SELECT
 DROP TABLE tmp_new_balance_update_event;
 DROP TABLE tmp_new_balance_key;
 DROP TABLE tmp_latest_balance;
+DROP TABLE tmp_new_latest_balance;
 DROP TABLE tmp_new_balance_update_with_latest_balance;
--- reserves/sql/on_reindex/00_setup_hypertables.sql
+++ reserves/sql/on_reindex/00_setup_hypertables.sql
@@ -22,6 +22,8 @@ CREATE UNIQUE INDEX balance_history_pkey
     ON balance_history USING btree (id ASC);
 ALTER TABLE balance_history
     ADD PRIMARY KEY USING INDEX balance_history_pkey;
+CREATE INDEX idx_balance_history_distinct
+    ON balance_history USING btree (asset_account, id DESC);
 CREATE INDEX idx_balance_history_asset_account
     ON balance_history USING hash (asset_account);
 create index idx_balance_history_asset_account_btree
@@ -39,37 +41,10 @@ CREATE UNIQUE INDEX supply_history_pkey
     ON supply_history USING btree (id ASC);
 ALTER TABLE supply_history
     ADD PRIMARY KEY USING INDEX supply_history_pkey;
+CREATE INDEX idx_supply_history_distinct
+    ON supply_history USING btree (asset_id, id DESC);
 CREATE INDEX idx_supply_history_asset_id
     ON supply_history USING hash (asset_id);
 
 SELECT create_hypertable('supply_history', 'id', if_not_exists := TRUE);
 SELECT set_chunk_time_interval('supply_history', 100000::BIGINT << 18);
-
-
-alter table balance_update_event
-set access method hypercore;
-alter table balance_update_event
-set (
-    timescaledb.orderby = 'id',
-    timescaledb.segmentby = 'asset_account'
-);
-
-alter table balance_history
-set access method hypercore;
-alter table balance_history
-set (
-    timescaledb.orderby = 'id',
-    timescaledb.segmentby = 'asset_account'
-);
-
-alter table supply_history
-set access method hypercore;
-alter table supply_history
-set (
-    timescaledb.orderby = 'id',
-    timescaledb.segmentby = 'asset_id'
-);
-
-CALL add_columnstore_policy('balance_update_event', after => 100000::BIGINT<<17-2);
-CALL add_columnstore_policy('balance_history', after => 100000::BIGINT<<17-2);
-CALL add_columnstore_policy('supply_history', after => 100000::BIGINT<<18-2);
