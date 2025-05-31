73eaa5b0962389f6346c28b3142a57edbfc74d84
Lev Gorodetskiy <github@droserasprout.space>
2025-05-31 20:13:09 -0300

    fix catch_exceptions wrapper

--- models/__init__.py
+++ models/__init__.py
@@ -44,11 +44,11 @@ async def catch_exceptions(
         yield
     except Exception as exception:
         event_arg = handler.args[0]
-        event_id = f'{event_arg.data.index}_{event_arg.data.extrinsic_index}'
+        event_id = f'{event_arg.data.level}-{event_arg.data.index}-{event_arg.data.extrinsic_index}'
         ctx.logger.error('%s: failed to process event `%s`, %s ', event_id, handler.config.callback, exception)
         value = {
             'payload': event_arg.payload,
-            'data': event_arg.data.model_dump(),
+            'data': event_arg.data.__dict__,
         }
         await Meta.create(
             key=f'fail_{event_id}',
