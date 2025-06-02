58f23180df3b29a5e529f0bdd3bce9b59a64ad88
Lev Gorodetskiy <github@droserasprout.space>
2025-06-02 03:33:26 -0300

    Revert "remove dipdup bug fixup"
    This reverts commit 2132908257d8e54311f5026bd4f86739aa8bf895.

--- handlers/batch.py
+++ handlers/batch.py
@@ -5,6 +5,8 @@ from datetime import datetime
 from datetime import timedelta
 from typing import TYPE_CHECKING
 
+from dipdup.config.substrate_events import SubstrateEventsHandlerConfig
+
 from dex_screener.models import Block
 from dex_screener.models import catch_exceptions
 
@@ -13,10 +15,71 @@ if TYPE_CHECKING:
     from dipdup.index import MatchedHandler
 
 
+class DeprecatedEvent:
+    names: tuple[str, ...] = NotImplemented
+    done: bool = False
+    level: int = NotImplemented
+
+
+class DexSwapEvent(DeprecatedEvent):
+    names = (
+        'Omnipool.BuyExecuted',
+        'Omnipool.SellExecuted',
+        'Stableswap.BuyExecuted',
+        'Stableswap.SellExecuted',
+        'XYK.BuyExecuted',
+        'XYK.SellExecuted',
+        'LBP.BuyExecuted',
+        'LBP.SellExecuted',
+        'OTC.Filled',
+        'OTC.PartiallyFilled',
+    )
+    level: int = 6837788
+
+
+class BroadcastSwapped(DeprecatedEvent):
+    names = ('Broadcast.Swapped',)
+    level: int = 7342919
+
+
+class BroadcastSwapped2(DeprecatedEvent):
+    names = ('Broadcast.Swapped2',)
+    level: int = 7582524
+
+
+deprecations = [  # Must be sorted by `level` value
+    DexSwapEvent,
+    BroadcastSwapped,
+    BroadcastSwapped2,
+]
+
+
 async def batch(
     ctx: HandlerContext,
     handlers: tuple[MatchedHandler, ...],
 ) -> None:
+    current_level = handlers[0].level
+    for deprecated in deprecations:
+        if current_level <= deprecated.level:
+            break
+        if deprecated.done:
+            continue
+        # Sweep all `hydradx_events` Index handlers and remove deprecated ones from Index Config (no more matched handlers until restart)
+        ctx.config.indexes['hydradx_events'].handlers = tuple(  # type: ignore[attr-defined]
+            [hc for hc in ctx.config.indexes['hydradx_events'].handlers if hc.name not in deprecated.names]  # type: ignore[attr-defined]
+        )
+        # Remove already matched handlers for deprecated events (just for current level)
+        handlers = tuple(
+            [
+                mh
+                for mh in handlers
+                if mh.index.name == 'hydradx_events'
+                and isinstance(mh.config, SubstrateEventsHandlerConfig)
+                and mh.config.name not in deprecated.names
+            ]
+        )
+        deprecated.done = True
+
     batch_levels: set[int] = set()
     for handler in handlers:
         if handler.level not in batch_levels:
