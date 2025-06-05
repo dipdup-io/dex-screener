f7ea23b2468118ef26e3f4367c580c700c697f20
Wizard1209 <wizard1209@pm.me>
2025-06-04 21:24:41 -0300

    Add api_proxy service and implement demo server script for testing

---
+++ scripts/proxy.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+
+import os
+from collections.abc import Callable
+from contextlib import asynccontextmanager
+from typing import Any
+
+import httpx
+import orjson
+from fastapi import APIRouter
+from fastapi import FastAPI
+from fastapi import Request
+from fastapi.responses import StreamingResponse
+from starlette.background import BackgroundTask
+
+"""
+Environment variables:
+- HTTP_CLIENT_HOST: Host of the HTTP client (default: hasura)
+- HTTP_CLIENT_PORT: Port of the HTTP client (default: 8080)
+- HTTP_SERVER_URL_PATH: URL path prefix for the API (default: /api/rest)
+- HTTP_SERVER_HOST: Host for the FastAPI server (default: 0.0.0.0)
+- HTTP_SERVER_PORT: Port for the FastAPI server (default: 8000)
+"""
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Manage HTTP client lifecycle"""
+    # TODO: add http client to state(async with yeild {client: cl}), add env variables with client host and port
+    client_host = os.getenv("HTTP_CLIENT_HOST", "hasura")
+    client_port = os.getenv("HTTP_CLIENT_PORT", "8080")
+    async with httpx.AsyncClient(base_url=f'http://{client_host}:{client_port}') as client:
+        yield {'client': client}
+
+app = FastAPI(lifespan=lifespan)
+base_route = APIRouter(prefix=os.getenv("HTTP_SERVER_URL_PATH", "/api/rest"))
+
+def remove_none_fields(data: Any) -> Any:
+    # TODO: remove reserves field when empty
+    # TODO: remove fields amount0 and amount1 for swap event type
+    # TODO: assetin and assetout 2 fields out of four should be presented (remove asset0In, asset0Out, asset1In, asset1Out if None)
+    return data
+
+def clean_json(data: Any) -> Any:
+    try:
+        json_data = orjson.loads(data)
+        cleaned_data = remove_none_fields(json_data)
+        return orjson.dumps(cleaned_data)
+    except orjson.JSONDecodeError:
+        print("Failed to decode JSON content")
+        pass
+
+
+async def forward_request(request: Request, transform: Callable[[bytes], bytes] | None = None) -> StreamingResponse:
+    # Forward request with exact headers and body
+    client: httpx.AsyncClient = request.state.client
+    url = httpx.URL(path=request.url.path)
+    # header filtering, if we will need it: headers = [(k, v) for k, v in request.headers.raw if k != b'host']
+    forwarded_request = client.build_request(
+        method=request.method,
+        url=url,
+        headers=request.headers.raw,
+        params=request.query_params,
+        # params included in the URL
+        content=request.stream()
+    )
+    print(f"Forwarding request to {forwarded_request.url}")
+    response = await client.send(forwarded_request, stream=True)
+
+    if transform:
+        # Read JSON content
+        content = await response.aread()
+        return StreamingResponse(
+            transform(content),
+            status_code=response.status_code,
+            headers=dict(response.headers),
+            background=BackgroundTask(response.aclose)
+        )
+
+    return StreamingResponse(
+        response.aiter_raw(),
+        status_code=response.status_code,
+        headers=dict(response.headers),
+        background=BackgroundTask(response.aclose)
+    )
+
+
+@base_route.get("/latest-block")
+async def forward_latest_block(request: Request):
+    return await forward_request(request)
+
+@base_route.get("/asset")
+async def forward_asset(request: Request):
+    return await forward_request(request, clean_json)
+
+@base_route.get("/pair")
+async def forward_pair(request: Request):
+    return await forward_request(request, clean_json)
+
+@base_route.get("/events")
+async def forward_events(request: Request):
+    return await forward_request(request, clean_json)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    app.include_router(base_route)
+
+    server_host = os.getenv("HTTP_SERVER_HOST", "0.0.0.0")
+    server_port = int(os.getenv("HTTP_SERVER_PORT", 8000))
+    uvicorn.run(app, host=server_host, port=server_port)
