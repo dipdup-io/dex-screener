c352793e4bbd6bd155020700f033b58f8ccc81b0
Lev Gorodetskiy <github@droserasprout.space>
2025-07-10 10:53:48 -0300

    additional events

--- handlers/hydradx/lbp/on_liquidity_added.py
+++ handlers/hydradx/lbp/on_liquidity_added.py
@@ -2,10 +2,12 @@ from dipdup.context import HandlerContext
 from dipdup.models.substrate import SubstrateEvent
 
 from dex_screener import models as models
+from dex_screener.handlers.hydradx import liquidity_added
 from dex_screener.types.hydradx.substrate_events.lbp_liquidity_added import LBPLiquidityAddedPayload
 
 
 async def on_liquidity_added(
     ctx: HandlerContext,
     event: SubstrateEvent[LBPLiquidityAddedPayload],
-) -> None: ...
+) -> None:
+    await liquidity_added(ctx, event)
--- handlers/hydradx/lbp/on_liquidity_removed.py
+++ handlers/hydradx/lbp/on_liquidity_removed.py
@@ -2,10 +2,12 @@ from dipdup.context import HandlerContext
 from dipdup.models.substrate import SubstrateEvent
 
 from dex_screener import models as models
+from dex_screener.handlers.hydradx import liquidity_removed
 from dex_screener.types.hydradx.substrate_events.lbp_liquidity_removed import LBPLiquidityRemovedPayload
 
 
 async def on_liquidity_removed(
     ctx: HandlerContext,
     event: SubstrateEvent[LBPLiquidityRemovedPayload],
-) -> None: ...
+) -> None:
+    await liquidity_removed(ctx, event)
