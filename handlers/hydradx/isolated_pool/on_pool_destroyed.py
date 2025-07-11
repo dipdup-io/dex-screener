8baec06bbb3c9e8cf417dbbc1f1a0c773d97d0a8
Lev Gorodetskiy <github@droserasprout.space>
2025-07-11 19:26:46 -0300

    fix destroyed pools

--- handlers/hydradx/isolated_pool/on_pool_destroyed.py
+++ handlers/hydradx/isolated_pool/on_pool_destroyed.py
@@ -2,6 +2,7 @@ from dipdup.context import HandlerContext
 from dipdup.models.substrate import SubstrateEvent
 
 from dex_screener import models as models
+from dex_screener.service.dex.isolated_pool.isolated_pool_service import destroyed_pools
 from dex_screener.types.hydradx.substrate_events.xyk_pool_destroyed import XYKPoolDestroyedPayload
 
 
@@ -9,4 +10,4 @@ async def on_pool_destroyed(
     ctx: HandlerContext,
     event: SubstrateEvent[XYKPoolDestroyedPayload],
 ) -> None:
-    pass
+    destroyed_pools.add(event.payload['pool'])
--- service/dex/isolated_pool/isolated_pool_service.py
+++ service/dex/isolated_pool/isolated_pool_service.py
@@ -18,21 +18,31 @@ if TYPE_CHECKING:
     from dex_screener.types.hydradx.substrate_events.xyk_pool_created import XYKPoolCreatedPayload
 
 
+destroyed_pools: set[str] = set()
+
+
 class IsolatedPoolService:
     logger = logging.getLogger('isolated_pool_service')
 
     @classmethod
     async def register_pool(cls, event: SubstrateEvent[XYKPoolCreatedPayload]):
         # NOTE: update because pool can be destroyed and recreated
-        pool, _ = await Pool.update_or_create(
-            account=event.payload['pool'],
-            defaults={
-                'dex_key': DexKey.IsolatedPool,
-                'dex_pool_id': event.payload['pool'],
-                'lp_token_id': event.payload['share_token'],
-                'shares': str(event.payload['initial_shares_amount']),
-            },
+        pool = await Pool.get_or_none(account=event.payload['pool'])
+        if pool:
+            assert pool.shares == '0'
+            assert pool._saved_in_db is True
+        else:
+            pool = Pool(account=event.payload['pool'])
+
+        pool.dex_key = DexKey.IsolatedPool
+        pool.dex_pool_id = event.payload['pool']
+        pool.lp_token_id = event.payload['share_token']
+        pool.shares = event.payload['initial_shares_amount']  # type: ignore[assignment]
+        await pool.save(
+            # NOTE: Special case: pool was destroyed in the same transaction
+            force_update=event.payload['pool'] in destroyed_pools,
         )
+
         cls.logger.info('Pool registered: %r.', pool)
 
         return pool
@@ -40,32 +50,29 @@ class IsolatedPoolService:
     @classmethod
     async def register_pair(cls, ctx, pool: Pool, event: SubstrateEvent[XYKPoolCreatedPayload]):
         pair_id = pool.account
-        if await Pair.exists(id=pair_id):
-            cls.logger.warning('Pair already exists: %s.', pair_id)
-            return
-
         asset_a = await Asset.get(id=event.payload['asset_a'])
         asset_b = await Asset.get(id=event.payload['asset_b'])
         event_info = DexScreenerEventInfoDTO.from_event(event)
 
-        pair = await Pair.create(
-            id=pair_id,
-            dex_key=DexKey.IsolatedPool,
-            asset_0_id=min(asset_a.id, asset_b.id),
-            asset_1_id=max(asset_a.id, asset_b.id),
-            pool=pool,
-            created_at_block_id=event_info.block_id,
-            created_at_tx_id=event_info.tx_index,
-            fee_bps=XYK_GET_EXCHANGE_FEE_BPS,
-        )
-        cls.logger.info('Pair registered in pool %r: %r.', pool, pair)
+        if not await Pair.exists(id=pair_id):
+            pair = await Pair.create(
+                id=pair_id,
+                dex_key=DexKey.IsolatedPool,
+                asset_0_id=min(asset_a.id, asset_b.id),
+                asset_1_id=max(asset_a.id, asset_b.id),
+                pool=pool,
+                created_at_block_id=event_info.block_id,
+                created_at_tx_id=event_info.tx_index,
+                fee_bps=XYK_GET_EXCHANGE_FEE_BPS,
+            )
+            cls.logger.info('Pair registered in pool %r: %r.', pool, pair)
 
-        await pool.assets.add(asset_a, asset_b)  # type: ignore[attr-defined]
-        cls.logger.info('Pair Assets added to pool %r: %s, %s.', pool, asset_a, asset_b)
+            await pool.assets.add(asset_a, asset_b)  # type: ignore[attr-defined]
+            cls.logger.info('Pair Assets added to pool %r: %s, %s.', pool, asset_a, asset_b)
 
         # FIXME: We need to fetch initial pool transfers to calculate share prices later
-        reserves_a_model = await AssetPoolReserve.get(pool=pool, asset=asset_a.id)
-        reserves_b_model = await AssetPoolReserve.get(pool=pool, asset=asset_b.id)
+        reserves_a_model = await AssetPoolReserve.get(pool=pool, asset=asset_a)
+        reserves_b_model = await AssetPoolReserve.get(pool=pool, asset=asset_b)
 
         node = ctx.datasources['node']
         interface: SubstrateInterface = node._interface
